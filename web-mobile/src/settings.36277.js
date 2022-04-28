441aae46de73438ebf4d506323fa6969b167c3be
potato <potato_kang@163.com>
2022-04-28 18:08:19 +0800

    更新

---
+++ web-mobile/src/settings.36277.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"504cf",main:"f56d5"}};
